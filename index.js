91f8657877b85fcd95b92eb02abcb3b6e4c66324
CGM123sherry <g.christinemurithi>
2024-06-23 14:41:59 +0300

    completed my arithmetic lab

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b) {
+    return a+b;
+  }
+
+ 
+
+  function subtract(a, b) {
+    return a - b;
+  }60 - 40; //=> 20
+
+  function multiply(a, b) {
+    return a * b;
+  }
+2 * 3.4; //=> 6.8
+
+
+function divide(a, b) {
+    return a / b;
+  }
+5.0 / 2.5; //=> 2
+
+function increment(n) {
+    return n + 1;
+  }
+  function decrement(n) {
+    return n - 1;
+  }
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
